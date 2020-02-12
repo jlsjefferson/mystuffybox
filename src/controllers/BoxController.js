1f5008d558d37f7fade9cdbc2ae5f24f2869a9fb
Jefferson Lúcio da Silva <jlsjefferson@gmail.com>
2020-02-11 21:53:15 -0300

    Termino Back-end

---
+++ src/controllers/BoxController.js
@@ -0,0 +1,23 @@
+const Box = require('../model/Box')
+
+
+class BoxController {
+    async store (req, res) {
+
+        const box = await Box.create(req.body)
+        return res.json(box);
+    }
+
+    async show (req, res){
+        const box = await Box.findById(req.params.id).populate({
+            path: 'files',
+            options: {sort: {createdAt: 1 }}
+        });
+
+
+        console.log(box)
+        res.json(Box);
+    }
+}
+
+module.exports = new BoxController();
